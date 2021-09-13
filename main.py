609d4d0191ec81dcf08d4f4dae734e7b792199c1
CameronChadwick <cameronchad25@gmail.com>
2021-09-13 09:35:31 -0400

    here are some oop notes

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-def
+class Rocket():
+    def __init__(self, x_loc, y_loc, height=200):
+        self.height = height
+        self.x_loc = x_loc
+        self.y_loc = y_loc
+        self.rocket_velo = 10
+
+    def move_up(self):
+        self.y_loc -= self.rocket_velo
+
+
+
+enemy_rockets = [Rocket() for i in range(10)]
+
+
+for rocket in enemy_rockets:
+    print(rocket)
